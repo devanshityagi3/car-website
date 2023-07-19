80d74c4a8f3ede20fb68ac4ae166ba799d68c4c4
Nitin-TTN <nitin1@tothenew.com>
2023-07-19 23:48:15 +0530

    Final

--- video.js
+++ video.js
@@ -1,32 +1,15 @@
-$(document).ready(function () {
-  const video = document.getElementById("video");
-  const playIcon = document.getElementById("playIcon");
-  const video2 = document.getElementById("video2");
-  let isPlaying = false;
-
-  // Function to toggle play/pause on the video
-  function togglePlayPause(video) {
-    if (isPlaying) {
-      video.pause();
-      isPlaying = false;
-      playIcon.src = "icons/play-button.png";
-    } else {
-      video.play();
-      isPlaying = true;
-      playIcon.src = "icons/pause-button.png";
+function togglePlayPause(vid, bid) {
+  var ppbutton = document.getElementById(bid);
+  ppbutton.addEventListener("click", playPause);
+  myVideo = document.getElementById(vid);
+  function playPause() {
+    if (myVideo.paused) {
+      myVideo.play();
+      ppbutton.src = "icons/pause-button.png";
+    }
+    else {
+      myVideo.pause();
+      ppbutton.src = "icons/play-button.png";
     }
   }
-
-  playIcon.addEventListener("click", togglePlayPause);
-
-  video.addEventListener("play", function () {
-    isPlaying = true;
-    playIcon.src = "icons/pause-button.png";
-  });
-
-  video.addEventListener("pause", function () {
-    isPlaying = false;
-    playIcon.src = "icons/play-button.png";
-  });
-});
-
+}
